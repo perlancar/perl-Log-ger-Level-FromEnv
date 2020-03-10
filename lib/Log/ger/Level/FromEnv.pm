dd22a6643ccbcdf019af109330d3d3e0714b5d81
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-03-10 20:59:31 +0700

    Use Log::ger::Util::set_level and be more flexible (pass any value from LOG_LEVEL to set_level() instead of only accepting predefined values)

--- lib/Log/ger/Level/FromEnv.pm
+++ lib/Log/ger/Level/FromEnv.pm
@@ -1,34 +1,22 @@
 package Log::ger::Level::FromEnv;
 
+# AUTHORITY
 # DATE
+# DIST
 # VERSION
 
-use Log::ger ();
+use Log::ger::Util;
 
-if (my $ll = $ENV{LOG_LEVEL}) {
-    if ($ll eq 'off' || $ll == 0) {
-        $Log::ger::Current_Level = 0;
-    } elsif ($ll eq 'fatal' || $ll == 10) {
-        $Log::ger::Current_Level = 10;
-    } elsif ($ll eq 'error' || $ll == 20) {
-        $Log::ger::Current_Level = 20;
-    } elsif ($ll eq 'warn' || $ll eq 'warning' || $ll == 30) {
-        $Log::ger::Current_Level = 30;
-    } elsif ($ll eq 'info' || $ll == 40) {
-        $Log::ger::Current_Level = 40;
-    } elsif ($ll eq 'debug' || $ll == 50) {
-        $Log::ger::Current_Level = 50;
-    } elsif ($ll eq 'trace' || $ll == 60) {
-        $Log::ger::Current_Level = 60;
-    }
+if (defined $ll = $ENV{LOG_LEVEL}) {
+    Log::ger::Util::set_level($ll);
 } elsif ($ENV{TRACE}) {
-    $Log::ger::Current_Level = 60;
+    Log::ger::Util::set_level("trace");
 } elsif ($ENV{DEBUG}) {
-    $Log::ger::Current_Level = 50;
+    Log::ger::Util::set_level("debug");
 } elsif ($ENV{VERBOSE}) {
-    $Log::ger::Current_Level = 40;
+    Log::ger::Util::set_level("info");
 } elsif ($ENV{QUIET}) {
-    $Log::ger::Current_Level = 20;
+    Log::ger::Util::set_level("error");
 }
 
 1;
@@ -51,6 +39,11 @@ log level to trace (60)), C<DEBUG> (if true, will set log level to debug (50)),
 C<VERBOSE> (if true, will set log level to info (40)), C<QUIET> (if true, will
 set log level to error (20)).
 
-This is convenient for CLI programs.
+This is convenient for CLI programs or one-liners.
+
 
 =head1 SEE ALSO
+
+L<Log::ger::App>
+
+L<Log::ger::Screen>
